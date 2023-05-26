dc5f7ad424773b05a1645ba0dba58c4ce70876d6
Abishekarchar <abishekrosario@gmail.com>
2023-05-26 10:51:32 +0530

    angular-lg-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { ProductsComponent } from './products/products.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponent,
+    ContentComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
